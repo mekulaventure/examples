e880cfe67b0f3ff70e6b15c50a8110e37157b7c5
emeka <anya18@stthomas.edu>
2018-02-24 03:34:49 +0000

    vagrantfile again

--- Vagrantfile
+++ Vagrantfile
@@ -5,5 +5,5 @@ Vagrant.configure(2) do |config|
 
   config.vm.box = "ubuntu/xenial64"
   config.vm.box_url = "/var/vagrant/boxes/xenial64.box"
-
+  config.vm.provision :shell, path: "./anya1857-Lab02.sh"
 end
--- starter.sh
+++ starter.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Starter shell script
 # Rename as YourID-Lab02.sh
+anya1857-Labo2.sh
+
 # Put commands below
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
